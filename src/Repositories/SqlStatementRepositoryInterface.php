89cdd065e551dec4282d9c3723988814c8eda328
Martin Eisenführer <m.eisenfuehrer@techdivision.com>
2021-09-16 19:09:30 +0200

    Cleanup code and 3.8.x compatibility

--- src/Repositories/SqlStatementRepositoryInterface.php
+++ src/Repositories/SqlStatementRepositoryInterface.php
@@ -3,17 +3,11 @@
 /**
  * TechDivision\Import\Dbal\Repositories\SqlStatementRepositoryInterface
  *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
- *
- * PHP version 5
+ * PHP version 7
  *
  * @author    Tim Wagner <t.wagner@techdivision.com>
  * @copyright 2021 TechDivision GmbH <info@techdivision.com>
- * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @license   https://opensource.org/licenses/MIT
  * @link      https://github.com/techdivision/import-dbal
  * @link      http://www.techdivision.com
  */
@@ -25,7 +19,7 @@ namespace TechDivision\Import\Dbal\Repositories;
  *
  * @author    Tim Wagner <t.wagner@techdivision.com>
  * @copyright 2021 TechDivision GmbH <info@techdivision.com>
- * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @license   https://opensource.org/licenses/MIT
  * @link      https://github.com/techdivision/import-dbal
  * @link      http://www.techdivision.com
  */
@@ -37,7 +31,7 @@ interface SqlStatementRepositoryInterface
      *
      * @var string
      */
-    const PK_MEMBER_NAME = 'pk_member_name';
+    public const PK_MEMBER_NAME = 'pk_member_name';
 
     /**
      * Returns the SQL statement with the passed ID.
